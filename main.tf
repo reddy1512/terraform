145889da91df5ebb9d37b0e446690157f6e70924
reddy1512 <bmk4297@gmail.com>
2026-03-22 12:02:45 +0530

    adding new files

---
+++ main.tf
@@ -0,0 +1,51 @@
+
+
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "my-subnet"
+  }
+}
+
+#resource "aws_security_group" "my_sg" {}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-02dfbd4ff395f2a1b"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.my_subnet.id
+
+  tags = {
+    Name = "my-instance"
+  }
+}
